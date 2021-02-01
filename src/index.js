aa4c91bdbf4efad1cb6c63023aa1421546f8de22
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-02 00:06:09 +0200

    init: Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  return array && array.length !== 0 ? Math.min(...array) : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return array && array.length !== 0 ? Math.max(...array) : 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array && array.length !== 0) {
+    let total = array.reduce((total, num) => {
+      return total += num
+    }, 0)
+    return total / array.length;
+  }
+  return 0
 }
